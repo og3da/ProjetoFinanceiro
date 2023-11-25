1f616aa388cd4aa557bdb47ee21a4c4706535d85
og3da <rapha488@gmail.com>
2023-11-25 14:34:00 -0300

    app login

---
+++ Projeto_django/login/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
