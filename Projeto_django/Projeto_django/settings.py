58ed31b64c3f6189440908d897bd61703a509770
og3da <rapha488@gmail.com>
2023-11-27 21:42:15 -0300

    pagina principal para direcionamento

--- Projeto_django/Projeto_django/settings.py
+++ Projeto_django/Projeto_django/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'projeto_EducacaoFinanceira',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'root',
+        'PASSWORD': 'admin',
         'HOST':'localhost',
         'PORT':'3306',
     } 
